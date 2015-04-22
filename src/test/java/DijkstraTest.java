cd6e6ad9492f9651ee353bf23aeb00e0d51aad00
Shengyuan Lu <sting_eleven@hotmail.com>
2015-04-22 22:27:57 +0800

    testDirect

--- src/test/java/DijkstraTest.java
+++ src/test/java/DijkstraTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class DijkstraTest {
 
     @Test
-    public void test1() {
+    public void testDirect() {
         Dijkstra dijkstra = new Dijkstra();
 
         dijkstra.graph.put(new Dijkstra.Node("A", 0), Arrays.asList(new Dijkstra.Node("B", 2), new Dijkstra.Node("C", 1)));
@@ -16,8 +16,10 @@ public class DijkstraTest {
         dijkstra.graph.put(new Dijkstra.Node("C", Integer.MAX_VALUE), Arrays.asList(new Dijkstra.Node("A", 1), new Dijkstra.Node("D", 3)));
         dijkstra.graph.put(new Dijkstra.Node("D", Integer.MAX_VALUE), Arrays.asList(new Dijkstra.Node("B", 2), new Dijkstra.Node("C", 3)));
 
-        System.out.println(dijkstra.dijkstra("A", "C"));
-
+        List<Dijkstra.Node> r = dijkstra.dijkstra("A", "C");  //A -> C
+        Dijkstra.Node nodeC = Iterables.getLast(r);
+        Assert.assertEquals("C", nodeC.name);
+        Assert.assertEquals(1, nodeC.distance);
     }
 
     @Test
