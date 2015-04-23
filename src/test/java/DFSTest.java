b1eb8890cf73281dfeca6319d710e64acdece3eb
Shengyuan Lu <sting_eleven@hotmail.com>
2015-04-23 21:57:20 +0800

    Add test2Nodes

--- src/test/java/DFSTest.java
+++ src/test/java/DFSTest.java
@@ -8,6 +8,21 @@ import java.util.List;
 public class DFSTest {
 
     @Test
+    public void test2Nodes() {
+        DFS dfs = new DFS();
+
+        Node a = new Node("A", 0);
+        Node b = new Node("B", 0);
+
+        dfs.graph.put(a, Arrays.asList(b));
+        dfs.graph.put(b, Arrays.asList(a));
+
+        List<Node> r = dfs.dfs("A");  //A -> B
+        Assert.assertEquals(a, r.get(0));
+        Assert.assertEquals(b, r.get(1));
+    }
+
+    @Test
     public void test() {
 
         DFS dfs = new DFS();
