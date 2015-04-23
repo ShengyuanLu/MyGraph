9b99b8b567c41b9edaa4e4a9dccbc29086e2a2e5
Shengyuan Lu <sting_eleven@hotmail.com>
2015-04-23 17:58:32 +0800

    update dfs test

--- src/test/java/DFSTest.java
+++ src/test/java/DFSTest.java
@@ -24,4 +24,26 @@ public class DFSTest {
         Assert.assertEquals(b, r.get(1));
         Assert.assertEquals(c, r.get(2));
     }
+
+    @Test
+    public void test1() {
+
+        DFS dfs = new DFS();
+
+        Node a = new Node("A", 0);
+        Node b = new Node("B", 0);
+        Node c = new Node("C", 0);
+        Node d = new Node("D", 0);
+
+        dfs.graph.put(a, Arrays.asList(b, c));
+        dfs.graph.put(b, Arrays.asList(a, d));
+        dfs.graph.put(c, Arrays.asList(a));
+        dfs.graph.put(d, Arrays.asList(b));
+
+        List<Node> r = dfs.dfs("A");  //A -> C -> B -> D
+        Assert.assertEquals(a, r.get(0));
+        Assert.assertEquals(c, r.get(1));
+        Assert.assertEquals(b, r.get(2));
+        Assert.assertEquals(d, r.get(3));
+    }
 }
