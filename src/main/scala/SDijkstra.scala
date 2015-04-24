e58aa6e64d5634fce2b02fd448497f05c1afe5a9
Shengyuan Lu <sting_eleven@hotmail.com>
2015-04-24 18:24:46 +0800

    Fix bug SDijkstra

--- src/main/scala/SDijkstra.scala
+++ src/main/scala/SDijkstra.scala
@@ -2,36 +2,31 @@ import scala.collection.mutable.LinkedHashMap
 
 object SDijkstra {
 
-
-
-  def dijkstra(graph: Map[SNode, List[SNode]], start: String, end: String): List[Node] = {
+  def dijkstra(graph: Map[SNode, List[SNode]], start: String, end: String): List[SNode] = {
     val path = new LinkedHashMap[SNode, SNode]
-    var bigSQueue = List[SNode]()
-    bigSQueue ++ graph.keySet.filter(_.name != start)
-
-    var top: SNode = graph.keySet.find(_.name == start).get
+    var bigSQueue = graph.keySet.filter(_.name != start).toList
+    var top = graph.keySet.find(_.name == start).get
     path += (top -> null)
 
-    while (!bigSQueue.isEmpty) {
-      val adj: List[SNode] = graph.get(top).filter(bigSQueue.contains(_)).get
+    while (bigSQueue.nonEmpty) {
+      val adj = graph.get(top).get.intersect(bigSQueue)
       adj.foreach(
         (v) => {
-
-          val candi: SNode = bigSQueue.find(_ == v).get
+          val candi = bigSQueue.find(_ == v).get
           val sum = top.distance + v.distance
           candi.distance = if (sum < candi.distance) sum
           else candi.distance
         }
-
       )
 
       top = bigSQueue.minBy(_.distance)
       path += (top -> path.keySet.last)
       if (top.name == end) {
-        if (top.distance == Integer.MAX_VALUE) List(top)
-
+        return if (top.distance == Int.MaxValue) List(top)
         else parse(path, top)
       }
+
+      bigSQueue = bigSQueue.diff(List(top))
     }
     null
   }
@@ -39,13 +34,11 @@ object SDijkstra {
   def parse(path: LinkedHashMap[SNode, SNode], end: SNode): List[SNode] = {
     var result = List[SNode]()
     var n = end
-    while (path.get(n) != null) {
+    while (path.get(n) != None) {
       result = result :+ n
       n = path.get(n).get
     }
-    result = result :+ path.keySet.head
-
-    return result.reverse
+    result.reverse
   }
 
 }
--- src/test/scala/SDijkstraTest.scala
+++ src/test/scala/SDijkstraTest.scala
@@ -2,17 +2,34 @@
 import org.junit.{Test, Assert}
 
 class SDijkstraTest {
-  @Test def testDirect() {
-    var graph = Map(
+
+  @Test
+  def testDirect() {
+
+    val graph = Map(
       new SNode("A", Int.MaxValue) -> List(new SNode("B", 2), new SNode("C", 1)),
       new SNode("B", Int.MaxValue) -> List(new SNode("A", 2), new SNode("D", 2)),
       new SNode("C", Int.MaxValue) -> List(new SNode("A", 1), new SNode("D", 3)),
       new SNode("D", Int.MaxValue) -> List(new SNode("B", 2), new SNode("C", 3))
     )
 
-    var l = SDijkstra.dijkstra(graph, "A", "C")
-    println(l)
+    val l = SDijkstra.dijkstra(graph, "A", "C")
 
+     Assert.assertEquals(1, l.last.distance)
+  }
+
+  @Test
+  def testUnDirect() {
+
+    val graph = Map(
+      new SNode("A", Int.MaxValue) -> List(new SNode("B", 2), new SNode("C", 10)),
+      new SNode("B", Int.MaxValue) -> List(new SNode("A", 2), new SNode("D", 2)),
+      new SNode("C", Int.MaxValue) -> List(new SNode("A", 10), new SNode("D", 3)),
+      new SNode("D", Int.MaxValue) -> List(new SNode("B", 2), new SNode("C", 3))
+    )
+
+    val l = SDijkstra.dijkstra(graph, "A", "C")
+    Assert.assertEquals(10, l.last.distance)
 
   }
 
