19549dd086db71b5271752d56c0ddb5690508376
Shengyuan Lu <sting_eleven@hotmail.com>
2015-04-24 13:43:31 +0800

    Init SDijkstra.dijkstra

--- src/main/scala/SDijkstra.scala
+++ src/main/scala/SDijkstra.scala
@@ -1,6 +1,14 @@
+import scala.collection.mutable.{LinkedHashMap, ListBuffer}
+
 object SDijkstra {
 
-  def dijkstra(): List[String] = {
+  def dijkstra(graph: Map[SNode, List[SNode]], start: String, end: String): List[Node] = {
+    val path = new LinkedHashMap[SNode, SNode]
+    var bigSQueue = new ListBuffer[SNode]
+    bigSQueue ++ graph.keySet
+
+    val top:SNode = graph.keySet.find(_.name == start).get
+    path += (top -> null)
     null
   }
 
